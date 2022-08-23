3aa141e7c95a5f10141c085b81e2054025de596a
Niclas Hansen <32816311+UXinFront@users.noreply.github.com>
2022-08-23 17:00:31 +0200

    commit

---
+++ Gennemsnitsberegner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Gennemsnitsberegner;
+
+class Program
+{
+    static void TalInput ()
+    {
+        do 
+        {
+            Console.WriteLine("Indtast tal: ");
+            string TalIndtast = Console.ReadLine();
+        } while (true);
+              
+    }
+    static void Main(string[] args)
+    {
+        TalInput();
+    }
+
+}
